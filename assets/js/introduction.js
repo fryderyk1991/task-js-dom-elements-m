f0231362bb1b3f3fd43f04e14398613c4129da5d
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-07-30 11:24:03 +0200

    removed unnecessary if && removed unnecessary attribute from span

--- assets/js/introduction.js
+++ assets/js/introduction.js
@@ -5,11 +5,9 @@ if (toolTip) {
     const link = document.createElement("a");
     const span = document.createElement("span");
     link.setAttribute("href", tool.dataset.url);
-    span.setAttribute(tool.dataset.tooltipType, tool.dataset.tooltipType);
     link.appendChild(tool.firstChild);
     tool.appendChild(link);
     tool.appendChild(span);
-    if (span.hasAttribute(tool.dataset.tooltipType)) {
       if (tool.dataset.tooltipType === "text") {
         span.textContent = tool.dataset.tooltipContent;
         span.classList.add("tooltip__box", "tooltip__box--text");
@@ -20,6 +18,5 @@ if (toolTip) {
         span.classList.add("tooltip__box", "tooltip__box--image");
         img.classList.add("tooltip__image");
       }
-    }
   });
 }
