7eb60c51d2a67006a5ff674c6838eb8ceea32358
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-08-09 17:34:11 +0200

    fix issue

--- assets/js/introduction.js
+++ assets/js/introduction.js
@@ -1,22 +1,30 @@
-const toolTip = document.querySelectorAll(".tooltip");
-
-if (toolTip) {
+function createTooltipText(tool) {
+  const span = document.createElement("span");
+  tool.appendChild(span);
+  return span;
+}
+function createTooltipImage(span, tool) {
+  if (tool.dataset.tooltipType === "text") {
+    span.textContent = tool.dataset.tooltipContent;
+    span.classList.add("tooltip__box", "tooltip__box--text");
+  } else {
+    const img = document.createElement("img");
+    span.appendChild(img);
+    img.setAttribute("src", tool.dataset.tooltipContent);
+    span.classList.add("tooltip__box", "tooltip__box--image");
+    img.classList.add("tooltip__image");
+  }
+}
+function prepareTooltip() {
+  const toolTip = document.querySelectorAll(".tooltip");
   toolTip.forEach(function (tool) {
     const link = document.createElement("a");
-    const span = document.createElement("span");
     link.setAttribute("href", tool.dataset.url);
     link.appendChild(tool.firstChild);
     tool.appendChild(link);
-    tool.appendChild(span);
-      if (tool.dataset.tooltipType === "text") {
-        span.textContent = tool.dataset.tooltipContent;
-        span.classList.add("tooltip__box", "tooltip__box--text");
-      } else {
-        const img = document.createElement("img");
-        span.appendChild(img);
-        img.setAttribute("src", tool.dataset.tooltipContent);
-        span.classList.add("tooltip__box", "tooltip__box--image");
-        img.classList.add("tooltip__image");
-      }
+    const span = createTooltipText(tool);
+    createTooltipImage(span, tool)
+    console.log(span)
   });
 }
+// prepareTooltip()
--- assets/js/list.js
+++ assets/js/list.js
@@ -43,39 +43,52 @@ const list = [
   },
 ];
 
-const sectionArticle = document.querySelector(".article__list");
 
-if (sectionArticle) {
-  const ul = document.createElement("ul");
-  sectionArticle.appendChild(ul);
-  list.forEach(function (el) {
-    if (el.parentId === null) {
-      const li = document.createElement("li");
-      const a = document.createElement("a");
-      li.setAttribute("data-id", el.id);
-      a.setAttribute("href", el.link);
-      a.textContent = el.text;
-      ul.appendChild(li);
-      li.appendChild(a);
-    }
-  });
-  const listWithNull = ul.querySelectorAll("li");
-  listWithNull.forEach(function (item) {
-    const id = Number(item.dataset.id);
-    const children = list.filter(function (el) {
-      return el.parentId === id;
-    });
-    const ulDropdown = document.createElement("ul");
-    if (id !== 1) {
-      item.appendChild(ulDropdown);
-    }
-    children.forEach(function (li) {
-      const liInsideDropdown = document.createElement("li");
-      const aInsideLi = document.createElement("a");
-      aInsideLi.setAttribute("href", li.link);
-      aInsideLi.textContent = li.text;
-      ulDropdown.appendChild(liInsideDropdown);
-      liInsideDropdown.appendChild(aInsideLi);
-    });
-  });
+
+const sectionArticle = document.querySelector(".article__list");
+const ul = document.createElement("ul");
+sectionArticle.appendChild(ul);
+
+function createListItem(link, text) {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.setAttribute("href", link);
+  a.textContent = text;
+  ul.appendChild(li);
+  li.appendChild(a);
 }
+
+
+
+// const ul = document.createElement("ul");
+// sectionArticle.appendChild(ul);
+// list.forEach(function (el) {
+//   if (el.parentId === null) {
+//     const li = document.createElement("li");
+//     const a = document.createElement("a");
+//     li.setAttribute("data-id", el.id);
+//     a.setAttribute("href", el.link);
+//     a.textContent = el.text;
+//     ul.appendChild(li);
+//     li.appendChild(a);
+//   }
+// });
+// const listWithNull = ul.querySelectorAll("li");
+// listWithNull.forEach(function (item) {
+//   const id = Number(item.dataset.id);
+//   const children = list.filter(function (el) {
+//     return el.parentId === id;
+//   });
+//   const ulDropdown = document.createElement("ul");
+//   if (id !== 1) {
+//     item.appendChild(ulDropdown);
+//   }
+//   children.forEach(function (li) {
+//     const liInsideDropdown = document.createElement("li");
+//     const aInsideLi = document.createElement("a");           ////// => to zamienil na funkcje get children i wniej jako parametr podac to z liniki numer 57 do 63 i 77- 82
+//     aInsideLi.setAttribute("href", li.link);
+//     aInsideLi.textContent = li.text;
+//     ulDropdown.appendChild(liInsideDropdown);
+//     liInsideDropdown.appendChild(aInsideLi);
+//   });
+// });
